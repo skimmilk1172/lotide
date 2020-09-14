895d9398e5218d6c5b17630de9d8193e70701e8a
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-14 20:52:05 +0000

    Implementing Head, to get first array

---
+++ head.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(firstItem) {
+  return firstItem[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
