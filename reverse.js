7b843baf2f3fe02bdfad5da22cf543001eb0ee5e
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-15 00:56:54 +0000

    Reversing Strings

---
+++ reverse.js
@@ -0,0 +1,8 @@
+const arg = process.argv.slice(2);
+for (let i = 0; i < arg.length; i++) { 
+  let output = ''; 
+  for (let j = arg[i].length -1; j >= 0; j--) {
+    output += arg[i][j];
+  }  
+  console.log(output);
+} 
