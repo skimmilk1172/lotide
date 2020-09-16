d7bc52c77c5cf32f83c47e52a87633c358391719
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-16 00:14:46 +0000

    assertArrayEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1,arr2) {
+  const result = eqArrays(arr1,arr2);
+  if (!result) {
+    console.log(`False. These are not equal`);
+  } else {
+    console.log(`True. These are equal`);
+  }
+};
