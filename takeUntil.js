890681029732cdc5861928d2b7dd5ee97b7f0cb0
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-17 23:38:06 +0000

    takeuntil

---
+++ takeUntil.js
@@ -0,0 +1,39 @@
+const takeUntil = function(array, callback) {
+  array.forEach(function(item, index) {
+    if (callback(item)) {
+      array = array.slice(0, index);
+    } return {      
+    }
+  });
+  return array;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
