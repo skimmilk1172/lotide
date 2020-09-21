4cab8a58eca3cc56d738f6468cfa819b34054cdd
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-21 17:29:51 +0000

    Refactoring using imports and exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const eqArrays = require('./eqArrays');
 
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1,arr2) {
+const assertArraysEquals = function(arr1,arr2) {
   const result = eqArrays(arr1,arr2);
   if (!result) {
     console.log(`False. These are not equal`);
@@ -26,3 +8,5 @@ const assertArraysEqual = function(arr1,arr2) {
     console.log(`True. These are equal`);
   }
 };
+
+module.exports = assertArraysEquals
