660b4727f1d995418b7a3ad4e954cb65b5d19771
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-21 19:05:46 +0000

    Mocha and Chai revised

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,14 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]));
-assertArraysEqual(middle([1, 2, 3, 4])) // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+describe('#middle', () => {
+  it('should return 2 for 1,2,3', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it('should return [] for [1,2]', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it('should return 3 for 1,3,5', () => {
+    assert.deepEqual(middle([1,3,5]), [3]);
+  });
+})
