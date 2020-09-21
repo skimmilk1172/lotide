02400fe2c723345db087f1f61cc664636afcaff6
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-21 17:22:28 +0000

    Refactoring eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -19,4 +10,4 @@ const eqArrays = function(arr1,arr2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
