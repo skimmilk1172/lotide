692820f6ebada4d364ca73bffee2fde42f9ce434
Sean Kim <sean.sihun.kim@gmail.com>
2020-09-21 17:50:00 +0000

    Refactoring middle.js

--- middle.js
+++ middle.js
@@ -1,23 +1,7 @@
-const assertArraysEqual = function(arr1,arr2) {
-  const result = eqArrays(arr1,arr2);
-  if (!result) {
-    console.log(`False. These are not equal`);
-  } else {
-    console.log(`True. These are equal`);
-  }
-};
 
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+
 
 const middle = function(array) {
   let newArray = [];
@@ -31,4 +15,4 @@ const middle = function(array) {
   return newArray
 };
 
-console.log(middle([1, 2]));
+module.exports = middle;
